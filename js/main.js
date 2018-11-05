4f046b0faf871faa51d20693378f8eacd18f2708
greenman <caasilemroh@gmail.com>
2018-11-05 14:27:05 -0600

    Made it kind of work

--- js/main.js
+++ js/main.js
@@ -1,37 +1,63 @@
 const canvas = document.getElementById("canvas");
 const g = canvas.getContext("2d");
 
-var elements = [];
+var questions = [];
+var currentQuestion = 0;
 
-var levels = [];
-var level = 0;
-var currentLevel;
-
-function render(){
-	for(let i in elements){
+var Q = function(){
+	
+	this.loadedTime = -1;
+	
+	this.currentTime = 0;
+	
+	this.time = 7000;
+	this.question = arguments[0] || "Example question";
+	this.answers = arguments[1] || ["Example answer 0", "Example answer 1", "Example answer 2", "Example answer 3"];
+	
+	this.answer = null;
+	
+	this.setup = function(){
+		document.getElementById("question").textContent = this.question;
+		document.getElementById("b0").textContent = this.answers[0];
+		document.getElementById("b1").textContent = this.answers[1];
+		document.getElementById("b2").textContent = this.answers[2];
+		document.getElementById("b3").textContent = this.answers[3];
+		this.loadedTime = (new Date()).getTime();
+		//console.log(this.loadedTime);
+		this.timeDown();
+	}
+	
+	this.timeDown = function(){
+		
+		var temp = this;
+		
+		var k = setInterval(function(){
+			if( ((new Date()).getTime() - temp.loadedTime) < temp.time ){
+				temp.renderTimer();
+			}else{
+				clearInterval(k);
+				console.log("done");
+			}
+		}, 20);
 		
 	}
-}
-
-function update(){
-	for(let i in elements){
+	
+	this.renderTimer = function(){
 		
+		canvas.width = window.innerWidth;
+		canvas.height = 10 + "px";
+		
+		g.fillStyle = "rgb(51, 51, 51)";
+		g.fillRect(0,0,canvas.width, canvas.height);
+		
+		g.fillStyle = "rgb(0, 60, 240)";
+		g.fillRect( (new Date()).getTime() - this.loadedTime, 0, ((new Date()).getTime() - this.loadedTime)*2, 10 );
 	}
-}
-
-var Level = function(){
-	var args = Array.prototype.slice.call(arguments);
-	
-	this.name = args[0] || "levelName";
-	this.elements = args[1] || [];
-	
-	this.exits = args[2] || [];
-	
-	this.update(){}
 	
 }
 
-var gameLoop = setInterval(function(){
-	update();
-	render();
-}, 31);
+function answer(){
+	questions[currentQuestion].answer = arguments[0] || null;
+}
+
+questions.push( new Q("What is your favorite color?", ["red", "green", "blue", "fuscia"]) );
